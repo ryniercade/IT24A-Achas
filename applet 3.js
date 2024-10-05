697f796a74af4ab92fb3825cdb3132c9f92effa9
ryniercade <rynierachas08@gmail.com>
2024-10-05 18:23:59 +0300

    Added clearLogs

--- applet 3.js
+++ applet 3.js
@@ -14,4 +14,8 @@ class DataLogger{
         this.loggedData.push(time);
         this.updateCardContainer();
     }
+    clearLogs(){
+        this.loggedData = []; 
+        this.updateCardContainer();
+    }
 }
