b86734292db33ded7c9606778d29d6c2dc0fa428
ryniercade <rynierachas08@gmail.com>
2024-10-08 15:00:29 +0300

    Added Changes

--- applet 2.js
+++ applet 2.js
@@ -3,38 +3,119 @@ class LeafletMap {
     constructor(containerId, center, zoom) {
         this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
+
+        this.attendanceCountSC = 0;
+        this.attendanceCountBA = 0;
+        this.attendanceCountLab = 0;
+
+        this.markerCounts = {};
+        this.markers = [];
+
+        this.loggedData = []; 
+
+        this.btn = document.getElementById('btn');
+        this.btn1 = document.getElementById('btn1');
+        this.btn2 = document.getElementById('btn2');
+        this.btnclear = document.getElementById('btnclear');
+        this.logCountElement = document.getElementById('logCount');
+        this.logCount1Element = document.getElementById('logCountBA');
+        this.logCount2Element = document.getElementById('logCountCCS');
+        this.idContainer = document.getElementById('logContainer');
+
+        this.btn.addEventListener('click', () => this.dataSc());
+        this.btn1.addEventListener('click', () => this.dataLab());
+        this.btn2.addEventListener('click', () => this.dataBa());
+        this.btnclear.addEventListener('click', () => this.clearLogs());
     }
 
     initTileLayer() {
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Sample for new corales BSIT student'
         }).addTo(this.map);
     }
 
-    addMarker(lat, lng, message) {
-        const marker = L.marker([lat, lng]).addTo(this.map);
-        marker.bindPopup(message);
+       addMarker(lat, long, message){
+        const marker = L.marker([lat, long]).addTo(this.map)
+        this.markerCounts[message] = (this.markerCounts[message] || 0) + 1;
+        this.updateMarkerPopup(marker, message);
+        this.markers.push(marker);
     }
 
+    updateMarkerPopup(marker, message) {
+        const count = this.markerCounts[message];
+        marker.bindPopup(`${message}<br>Attendance logs: ${count}`).openPopup();
+    }
+
+    
     loadMarkersFromJson(url) {
         fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                data.forEach(marker => {
-                    this.addMarker(marker.latitude, marker.longitude, marker.message);
-                });
-            })
-            .catch(error => console.error('Error loading markers:', error));
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(marker => {
+                this.addMarker(marker.latitude, marker.longitude, marker.message);
+            });
+        })
+        .catch(error => console.error("Error Loading servers:", error));
     }
+    
+    clearLogs(){
+        this.attendanceCountSC = 0;
+        this.attendanceCountBA = 0;
+        this.attendanceCountLab = 0;
+
+        this.loggedData = [];
+        this.markerCounts = {}; 
+        this.markers.forEach(marker => {
+            const message = marker.getPopup().getContent().split('<br>')[0]; 
+            this.markerCounts[message] = 0;
+            this.updateMarkerPopup(marker, message); 
+        });
+
+        this.updateLogDisplay();
+    }
+
+    displayLogCount() {      
+        this.logCountElement.innerHTML = `SC Building Attendance: ${this.attendanceCountSC}`;
+        this.logCount1Element.innerHTML = `BA Building Attendance: ${this.attendanceCountBA}`;
+        this.logCount2Element.innerHTML = `CCS Laboratory Attendance: ${this.attendanceCountLab}`;
+   }
+
+    dataSc() {
+        this.addMarker(8.360238, 124.867470, 'SC building');
+        this.attendanceCountSC++; 
+        this.updateLogDisplay();
+    }
+
+    dataBa() {
+        this.addMarker(8.359134, 124.868537, 'BA building');
+        this.attendanceCountBA++;
+        this.updateLogDisplay();
+    }
+    
+    dataLab() {
+        this.addMarker(8.359639, 124.869179, 'CCS Laboratory 1');
+        this.attendanceCountLab++;
+        this.updateLogDisplay();
+    }
+
+    updateLogDisplay() {
+        this.idContainer.innerHTML = ''; 
+        this.loggedData.forEach(data => {
+            const logItem = document.createElement('div');
+            logItem.className = 'log-item';
+            this.idContainer.appendChild(logItem);
+        });
+        this.displayLogCount();
+    }
+
 }
-
-const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
-
-
-myMap.addMarker(8.359735, 124.869206, 'CCS Faculty Office');
-myMap.addMarker(8.359639,124.869179, 'CCS Laboratory 1');
-myMap.addMarker(8.359554,124.869153, 'CCS Laboratory 2');
+const Mymap = new LeafletMap('map', [8.359735, 124.869206], 18);
 
 
-myMap.loadMarkersFromJson('applet-2.json');
+Mymap.loadMarkersFromJson('applet-2.json');
+
+document.addEventListener('DOMContentLoaded', () => {
+    Mymap.displayLogCount();
+    Mymap.loadMarkersFromJson('applet-2.json');
+});
