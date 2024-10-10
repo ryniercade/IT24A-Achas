9cbcc66f25c3542bce02e9599f224eaeafc4e0ce
ryniercade <rynierachas08@gmail.com>
2024-10-10 22:04:15 +0300

    Added fetchData

--- applet 4.js
+++ applet 4.js
@@ -12,5 +12,13 @@ class StudentList {
         this.bindSearchEvent();
 
     }
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            this.students = await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        } 
+     }
 
 }
