ecff59c1710dacd2692d5e78c0c39fbf4a83cc59
ryniercade <rynierachas08@gmail.com>
2024-10-17 20:16:49 +0300

    added get weather btn

--- applet 5.js
+++ applet 5.js
@@ -1,6 +1,6 @@
 class WeatherApp{
     constructor(){
         this.cityInput = document.getElementById('cityInput');
-       
+        this.getWeatherBtn = document.getElementById('getWeatherBtn');
     }
 }
