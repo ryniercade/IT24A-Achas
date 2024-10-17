1a7b87f475ea8589d52ce3495b4af381e9dd5383
ryniercade <rynierachas08@gmail.com>
2024-10-17 20:28:38 +0300

    added async fetchweather

--- applet 5.js
+++ applet 5.js
@@ -31,4 +31,25 @@ class WeatherService extends WeatherApp {
         super();
         this.apiKey = '';
     }
+
+    async fetchWeather() {
+        const city = this.cityInput.value;
+        this.apiKey = this.APIKeyInput.value.trim(); 
+
+        if (!this.apiKey) {
+            alert('Please enter your API key.');
+            return;
+        }
+
+        if (city) {
+            const data = await this.getWeatherData(city);
+            if (data) {
+                this.displayWeather(data);
+            } else {
+                alert('City not found. Please try again.');
+            }
+        } else {
+            alert('Please enter a city name.');
+        }
+    }
 }
